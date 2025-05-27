cfe94828e2fadcf00855d7361a8ef09e23863984
Diego Caucota <diegocaucota@gmail.com>
2025-05-27 13:45:34 -0300

    fix: page.tsx title

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center py-2">
+    <main className="flex flex-col items-center py-2 mt-20 gap-4">
       <span className="text-5xl">Hola Mundo</span>
-      <Link href={'/about'}>About Page</Link>
+      <Link href={"/about"}>About Page</Link>
     </main>
   );
 }
